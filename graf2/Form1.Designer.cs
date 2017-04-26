09ff0595ca59d30cdd2a9c8bee7c0da95eca22dc
Sydnej <sydnej.w@gmail.com>
2017-04-26 18:10:59 +0200

    git

---
+++ graf2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace graf2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button_count = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(13, 13);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "Open GXL";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button_count
+            // 
+            this.button_count.Location = new System.Drawing.Point(95, 12);
+            this.button_count.Name = "button_count";
+            this.button_count.Size = new System.Drawing.Size(75, 23);
+            this.button_count.TabIndex = 1;
+            this.button_count.Text = "Count";
+            this.button_count.UseVisualStyleBackColor = true;
+            this.button_count.Click += new System.EventHandler(this.button_count_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(84, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 31);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 135);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_count);
+            this.Controls.Add(this.button_open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button_count;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ graf2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace graf2
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Dictionary<string, int>> graph;
+        XmlDocument gxl = new XmlDocument();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "GXL Files|*.gxl";
+            openFileDialog1.Title = "Select a GXL File";
+            Dictionary<string,Dictionary<string,int>> colors = new Dictionary<string, Dictionary<string, int>>();
+
+            graph = new Dictionary<string, Dictionary<string, int>>();
+
+            Dictionary<string, int> colorNumber = new Dictionary<string, int>();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                gxl.Load(openFileDialog1.OpenFile());
+
+                XmlNodeList elemList = gxl.GetElementsByTagName("edge");
+                for (int i = 0; i < elemList.Count; i++)
+                {
+                    string from = elemList[i].Attributes["from"].Value;
+                    string to = elemList[i].Attributes["to"].Value;
+
+                    if (!graph.ContainsKey(from))
+                    {
+                        graph.Add(from, new Dictionary<string, int>());
+                        graph[from].Add(to, 0);
+                    }
+                    else
+                    {
+                        if (!graph[from].ContainsKey(to))
+                        {
+                            graph[from].Add(to, 0);
+                        }
+                    }
+
+                    if (!graph.ContainsKey(to))
+                    {
+                        graph.Add(to, new Dictionary<string, int>());
+                        graph[to].Add(from, 0);
+                    }
+                    else
+                    {
+                        if (!graph[to].ContainsKey(from))
+                        {
+                            graph[to].Add(from, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button_count_Click(object sender, EventArgs e)
+        {
+            int max_color = 0;
+            foreach (var node in graph)
+            {
+                int color = node.Value.Count + 1;
+                if (color > max_color) max_color = color;
+            }
+
+            label1.Text = max_color.ToString();v
+        }
+    }
+}
